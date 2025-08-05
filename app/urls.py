e86c1ac5a72bdfe22169faf850da318bb5a343ef
msantos7gabriel <gabriel.msantos7@hotmail.com>
2025-08-05 12:47:25 -0300

    half admin pannel done

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('index/', include('home.urls')),
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('community/', include('community.urls')),
     path('post/', include('post.urls')),
--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('courses/', views.courses, name='courses'),
     path('trainers/', views.trainers, name='trainers'),
     path('events/', views.events, name='events'),
     path('contact/', views.contact, name='contact'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin-dashboard'),
 ]
