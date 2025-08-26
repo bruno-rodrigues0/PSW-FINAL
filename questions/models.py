33852b11310959b771200c3b55eb2ab2aa1b18a3
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-08-26 11:20:03 -0300

    fix questions models

--- questions/models.py
+++ questions/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from topics.models import Topics
 
-
 class Questions(models.Model):
     statement = models.CharField(max_length=1000)
     alternative_a = models.BooleanField()
@@ -12,7 +11,6 @@ class Questions(models.Model):
     alternative_e = models.BooleanField()
     topic = models.ManyToManyField(Topics, through='Question_Topic')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
 
 class Question_Topic(models.Model):
     question = models.ForeignKey(Questions, on_delete=models.CASCADE)
